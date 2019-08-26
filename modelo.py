44b4748e78f738625e7218e18425ce3e780a37a1
adriano <adriano_cardoso>
2019-08-26 20:38:42 -0300

    incluir rota, veiculo, reserva

--- modelo.py
+++ modelo.py
@@ -9,3 +9,25 @@ if __name__=="__main__":
     v2=Veiculo("DEF-0001","VW","Gol","veiculo cambio manual")
     print(v1.placa+", "+v1.marca,","+v1.modelo,", "+v1.observacao)
     print(v2.placa+", "+v2.marca,","+v2.modelo,", "+v2.observacao)
+
+class Rota:
+    def __init__ (self, codRota, partida, destino):
+        self.codRota=codRota
+        self.partida=partida
+        self.destino=destino
+if __name__=="__main__":
+    r1=Rota("Rota001","Blumenau","Gaspar")
+    r2=Rota("Rota002","Blumenau","Brusque")
+    print(r1.codRota+", "+r1.partida,","+r1.destino)
+    print(r2.codRota+", "+r2.partida,","+r2.destino)
+
+class Reserva:
+    def __init__ (self,placa, data, horario, rota, obsMotorista):
+        self.placa=placa
+        self.data=data
+        self.horario=horario
+        self.rota=rota
+        self.obsMotorista=obsMotorista
+if __name__=="__main__":
+    rs1=Reserva("ABC-0000","01/01/2019","10:00","Rota001","com Motorista")
+    print(rs1.placa+", "+rs1.data+", "+rs1.horario+", "+rs1.obsMotorista)        
--- trabalho01.py
+++ trabalho01.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, redirect
-from modelo import Veiculo
+from modelo import Veiculo, Rota, Reserva
 
 listaVeiculo=[]
+listaRota=[]
+listaReservas=[]
 
 app=Flask(__name__, static_url_path='',static_folder='templates')
 
 @app.route("/")
 def realizarReserva():
-    return render_template('realizarReserva.html')
+    return render_template('realizarReserva.html',listaVeiculo = listaVeiculo)
 
 @app.route("/gerenciarVeiculo")
 def gerenciarVeiculo():
-    return render_template('gerenciarVeiculo.html', listaVeiculo = listaVeiculo)
+    return render_template('gerenciarVeiculo.html',listaVeiculo = listaVeiculo)
 
 @app.route("/incluirVeiculo", methods=['post'])
 def incluirVeiculo():
@@ -25,11 +27,31 @@ def incluirVeiculo():
 
 @app.route("/gerenciarReserva")
 def gerenciarReserva():
-    return render_template('gerenciarReserva.html')
+    return render_template('gerenciarReserva.html',listaReservas = listaReservas)
+
+@app.route("/incluirReserva", methods=['post'])
+def incluirReserva():
+    placa = request.form["veiculo"]
+    data = request.form["dataAgendamento"]
+    horario = request.form["horarioSaida"]
+    rota = request.form["destinoViagem"]
+    obsMotorista = request.form["motorista"]
+    nova=Reserva(placa,data,horario,rota,obsMotorista)
+    listaReservas.append(nova)
+    return redirect("/")
 
 @app.route("/cadastrarRota")
 def cadastrarRota():
-    return render_template('cadastrarRota.html')
+    return render_template('cadastrarRota.html',listaRota = listaRota)
+
+@app.route("/incluirRota", methods=['post'])
+def incluirRota():
+    codRota = request.form["codRota"]
+    partida = request.form["partida"]
+    destino = request.form["destino"]
+    novaRota = Rota(codRota,partida,destino)
+    listaRota.append(novaRota)
+    return redirect("/cadastrarRota")
 
 @app.route("/imgMapaNavegacao")
 def imgMapaNavegacao():
